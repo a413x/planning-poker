0cab33aeaa810c6cefaada69c3b84593895b4899
a413x <variuhin75@gmail.com>
2022-01-06 14:17:22 +0300

    add text button component

---
+++ src/components/elements/btn/TextButton.vue
@@ -0,0 +1,29 @@
+<template>
+    <button
+        class="text-button"
+        :disabled="disabled"
+        @click="$emit('click')"
+    >
+        <slot></slot>
+    </button>
+</template>
+
+<script>
+export default {
+    name: "TextButton",
+    props: { disabled: Boolean }
+}
+</script>
+
+<style scoped lang="less">
+.text-button{
+    color: white;
+    background: transparent;
+    box-shadow: none;
+    user-select: none;
+    cursor: pointer;
+    &:hover{
+        color: lightgray;
+    }
+}
+</style>
