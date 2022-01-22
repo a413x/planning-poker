795e7d46bb852c61f620add3254ad19f27160e99
a413x <variuhin75@gmail.com>
2022-01-22 14:02:54 +0300

    add user-form component

---
+++ src/components/user-form/UserForm.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="user-form d-flex justify-center">
+        <div class="d-flex flex-column">
+            Avatar
+            <v-avatar tile size="50" color="grey" />
+        </div>
+        <div class="d-flex flex-column ml-5">
+            User name
+            <v-text-field class="user-form__input" v-model="name" solo />
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "UserForm",
+    props: ["name"]
+}
+</script>
+
+<style scoped lang="scss"></style>
