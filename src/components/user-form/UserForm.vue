ceb35c0633e095e376837c309091e9f5852cf645
a413x <variuhin75@gmail.com>
2022-11-19 13:03:11 +0300

    adjust user form to receive avatar from props and change in on name change

--- src/components/user-form/UserForm.vue
+++ src/components/user-form/UserForm.vue
@@ -2,11 +2,18 @@
     <div class="user-form d-flex justify-center">
         <div class="d-flex flex-column">
             Avatar
-            <v-avatar tile size="50" color="grey" />
+            <v-avatar size="50" :color="avatar && avatar.color || 'grey'">
+                <span class="white--text">{{avatar && avatar.text || ""}}</span>
+            </v-avatar>
         </div>
         <div class="d-flex flex-column ml-5">
             User name
-            <v-text-field class="user-form__input" v-model="name" solo />
+            <v-text-field
+                class="user-form__input"
+                :value="name"
+                @input="onInput"
+                solo
+            />
         </div>
     </div>
 </template>
@@ -14,7 +21,22 @@
 <script>
 export default {
     name: "UserForm",
-    props: ["name"]
+    props: ["name", "avatar"],
+    methods: {
+        onInput(value) {
+            const data = {
+                name: value,
+                avatar: this.avatar
+            }
+            if(value) {
+                data.avatar = {
+                    ...this.avatar,
+                    text: value[0]
+                }
+            }
+            this.$emit("on-name-change", data)
+        }
+    }
 }
 </script>
 
